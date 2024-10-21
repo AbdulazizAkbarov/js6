a0645d1012b6fdfeb804215604e05550dd57e121
Abdulaziz <abdulazizakbarov065@gmail.com>
2024-10-21 19:29:56 +0500

    Salom

---
+++ app.js
@@ -0,0 +1,201 @@
+//1-Misol//
+
+// let matn = prompt("Soz Kirgizing");
+
+// for (let i = matn.length-1; i>=0; i--) {
+
+//     console.log(matn[i]);
+// }
+
+//1-Tugashi//
+
+//2-Misol//
+
+// let son =prompt("Soz Kirgizing");
+
+// for (let i=son.length; i>=0; i--) {
+//     alert(son[i]);
+// }
+
+//2-Tugashi//
+
+
+
+//3-Misol//
+
+// let son= prompt("Son Kiriting");
+
+
+// for(let i=1 ; i<=son; i++){
+
+//     if (i%2==0) {
+
+//         console.log(i+" Juft");
+     
+//     }
+//     else if (i%2!=0) {
+
+//     console.log(i+" Toq");
+    
+// }
+// }
+
+//3-Tugashi//
+
+
+
+//4-Misol//
+
+
+// let n= +prompt("Son Kirgzing");
+
+// let summa = 0;
+
+// for (let i=1 ; i<n; i++) {
+
+//     if( n %i==0){
+//         summa++;
+
+//     }
+
+// }
+
+// if (summa==1){
+
+//     alert("Tub Son")
+// }
+// else{
+//     alert("Murakkab Son")
+// }
+
+
+
+//4-Tugashi//
+
+
+//5-Misol//
+
+// let son =prompt("Son Kirgizing");
+// let summa = 0;
+// for (let i=1 ; i<son;i++) {
+
+// if (son%i==0) {
+
+//     summa += i;
+// }
+
+// }
+
+// if (summa==son){
+
+//     alert("Mukammal son")
+// }
+// else{
+//     alert("Mukammal Son emas")
+// }
+
+
+//5-Tugashi//
+
+
+//6-Misol// 
+
+
+
+// let son =+prompt("3-Xonalik Son kirgizing");
+
+
+// let birlik =son % 10;
+// let onlik =(son/10)%10;
+// let yuzlik =son / 100;
+
+
+// let ozgaruvchi = birlik + onlik + yuzlik;
+
+// let ozgaruvchi2 = ozgaruvchi / 10;
+
+// let ozgaruvchi3 = ozgaruvchi % 10;
+
+// let jami = ozgaruvchi2 + ozgaruvchi3;
+
+// alert(parseInt(jami));
+
+
+//6-Tugashi//
+
+
+//7-Misol//
+
+// let arr = [8,9,2,8,32,52,12,35,87,95,102,596];
+
+// let kotta =arr.sort((a,b)=>a-b);
+
+// alert(kotta[kotta.length-1]);
+
+
+//7-Tugashi//
+
+
+//8-Misol//  
+
+// let matn = prompt("Soz Kirgizing");
+// let summa= "";
+// for (let i = matn.length-1; i>=0; i--) {
+
+//     summa +=matn[i];
+// }
+
+// if(summa===matn){
+//     alert("True");
+// }else{
+
+//     alert("False")
+// }
+
+
+//8-Tugashi//
+
+
+/////////////////////////////////////////////QOSHIMCHA//////////////////////
+
+// let k = prompt("K-Sonini kirgizing");
+
+// let n = prompt("N-Sonini kirgizing");
+
+
+// for (let i =1; i<=n ;i++) {
+
+//     alert(k);
+// }
+
+
+
+
+// let a =prompt("A-Sonini Kirgizing");
+
+// let b =prompt("B-Sonini Kirgizing");
+
+
+// for(let i = a; i <= b; i++){
+//     console.log(i);
+    
+// }
+
+
+
+// let a =prompt("A-Sonini Kirgizing");
+
+// let b =prompt("B-Sonini Kirgizing");
+
+
+// for(let i = b; i >=a; i--){
+//     console.log(i);
+    
+// }
+
+
+
+
+
+
+
